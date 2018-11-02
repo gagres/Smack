63fb5cc41e6e2af5305ec0d89275232a5335b1e3
Gabriel Giro Resende <gabriel.giro.resende@outlook.com>
2018-11-02 01:24:45 -0300

    Lesson 2

--- app/src/main/java/com/example/gabri/smack/MainActivity.kt
+++ app/src/main/java/com/example/gabri/smack/MainActivity.kt
@@ -8,6 +8,7 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
@@ -33,13 +34,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_settings -> return true
-            else -> return super.onOptionsItemSelected(item)
-        }
+    fun loginBtnNavClicked(view: View) {
+
+    }
+
+    fun addChannelClicked(view: View) {
+
+    }
+
+    fun sendMessageBtnClicked(view: View) {
+
     }
 }
